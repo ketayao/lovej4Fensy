d7b4090c924a1b71ad6bd651ea510da2ea06f8a3
ketayao <ketayao@gmail.com>
2013-10-13 21:33:18 +0800

    1.添加自动跳转到后台首页。2.添加百度统计。3.添加评论邮件回复。4.修复邮件发送乱码。
    Signed-off-by: ketayao <ketayao@gmail.com>

--- src/main/java/com/ketayao/action/CommentAction.java
+++ src/main/java/com/ketayao/action/CommentAction.java
@@ -13,19 +13,22 @@
  
 package com.ketayao.action;
 
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Timestamp;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.mail.HtmlEmail;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
 import com.ketayao.fensy.mvc.IUser;
 import com.ketayao.fensy.mvc.RequestContext;
 import com.ketayao.fensy.webutil.ImageCaptchaService;
+import com.ketayao.pojo.Article;
 import com.ketayao.pojo.Comment;
+import com.ketayao.pojo.SiteConfig;
 import com.ketayao.system.Constants;
+import com.ketayao.system.SystemConfig;
 
 /** 
  * 	
@@ -40,12 +43,11 @@ public class CommentAction extends AbstractAction {
 	 * @param rc
 	 * @param p
 	 * @return  
-	 * @throws InvocationTargetException 
-	 * @throws IllegalAccessException 
+	 * @throws Exception 
 	 * @see com.ketayao.action.AbstractAction#process(com.ketayao.fensy.mvc.RequestContext, java.lang.String[])  
 	 */
 	@Override
-	protected String process(RequestContext rc, String[] p) throws IllegalAccessException, InvocationTargetException {
+	protected String process(RequestContext rc, String[] p) throws Exception {
 		boolean correct = ImageCaptchaService.validate(rc.getRequest());
 		if (!correct) {
 			rc.setRequestAttr("exception", "验证码错误");
@@ -83,7 +85,11 @@ public class CommentAction extends AbstractAction {
 			pageIndex = NumberUtils.toInt(p[0], 1) + 1;//加一，超过最大页数，默认为最后一页
 		}
 		
-		return "redirect:" + rc.getContextPath() + "/view/" + comment.getArticleId() + "/" + pageIndex + "#comments";
+		String view ="/view/" + comment.getArticleId() + "/" + pageIndex + "#comments";
+		
+		sendMail(comment, view, rc);
+		
+		return "redirect:" + rc.getContextPath() + view;
 	}
 	
 	private void saveCommentUserInCookie(RequestContext rc, Comment comment) {
@@ -97,4 +103,44 @@ public class CommentAction extends AbstractAction {
 		rc.setCookie(Constants.COMMENT_USER, uuid, RequestContext.MAX_AGE, true);
 	}
 	
+	private void sendMail(Comment comment, String view, RequestContext rc) throws Exception {
+		SiteConfig siteConfig = (SiteConfig)rc.getContext().getAttribute(Constants.SITE_CONFIG);
+		
+		HtmlEmail email = new HtmlEmail();
+		email.setStartTLSEnabled(true);
+		email.setHostName(SystemConfig.getConfig().get("blog.exception.email.hotname"));
+		email.setAuthentication(SystemConfig.getConfig().get("blog.exception.email.name"), 
+				SystemConfig.getConfig().get("blog.exception.email.password"));
+		email.setFrom(SystemConfig.getConfig().get("blog.exception.email.name"), siteConfig.getName());
+		email.setCharset("utf-8");
+		
+		Article article = comment.getArticle();
+		
+		StringBuilder content = new StringBuilder();
+		content.append("<table width=\"100%\"><tbody>");
+		content.append("<tr><td><h2>原文地址：<a href=\"" + SystemConfig.getConfig().get("lovej.site.url") + view 
+				+ "\" target=\"_blank\">" + article.getTitle() + "</a></h2></td></tr>");
+		if (comment.getParentId() != null) {
+			// 回复
+			Comment parentComment = comment.getParent();
+			content.append("<tr><td>" + parentComment.getContent() + "</td></tr>");
+			email.setSubject("你对《" + article.getTitle() +  "》的评论得到回复");
+			
+			email.addTo(parentComment.getEmail());
+		} else {
+		    // 新评论
+			email.setSubject("《" + article.getTitle() +  "》有新评论");
+			
+			email.addTo(article.getUser().getEmail());
+		}
+		content.append("<tr><td style=\"font-style:italic;\">&nbsp;&nbsp;&nbsp;&nbsp;" + comment.getContent() + "</td></tr>");
+		content.append("</tbody></table>");
+		
+		// set the html message
+		email.setHtmlMsg(content.toString());
+		
+		// set the alternative message
+		email.setTextMsg("Your email client does not support HTML messages");
+		email.send();
+	}
 }
