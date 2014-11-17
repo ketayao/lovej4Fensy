024917934e23f219e1d88d1f635454e48ee447de
ketayao <ketayao@gmail.com>
2014-11-17 09:58:52 +0800

    添加使用说明，修改一些小bug。

--- src/main/java/com/ketayao/action/admin/LinkAction.java
+++ src/main/java/com/ketayao/action/admin/LinkAction.java
@@ -10,7 +10,7 @@
  *
  * </pre>
  **/
- 
+
 package com.ketayao.action.admin;
 
 import java.lang.reflect.InvocationTargetException;
@@ -31,77 +31,78 @@ import com.ketayao.util.PageInfo;
 
 public class LinkAction {
 
-	private final static String CREATE = "admin/contact/link-create";
-	private final static String READ = "admin/contact/link-read";
-	private final static String UPDATE = "admin/contact/link-update";
-	//private final static String DELETE = "admin/contact/link-read";
-	
-	public String pc() {
-		return CREATE;
-	}
-	
-	public String c(WebContext rc) throws IllegalAccessException, InvocationTargetException {
-		Link link = new Link();
-		rc.populate(link);
-		
-		if (link != null && link.getName() != null) {
-			link.setCreateTime(new Timestamp(System.currentTimeMillis()));
-			link.save();
-			
-			rc.setRequestAttr(Constants.OPERATION_SUCCESS, Constants.OPERATION_SUCCESS);
-		}
-		
-		return CREATE;
-	}
-	
-	/**
-	 * 
-	 * 当参数地址相同时pageInfo不用rc.setRequestAttr,但是严谨性,请将所需参数手动加入map
-	 * @param map
-	 * @param pageInfo
-	 * @return
-	 * @throws InvocationTargetException 
-	 * @throws IllegalAccessException 
-	 */
-	public String r(WebContext rc) throws IllegalAccessException, InvocationTargetException {
-		PageInfo pageInfo = new PageInfo();
-		rc.populate(pageInfo);
-		
-		List<Link> links = Link.INSTANCE.findPage(pageInfo, false);
-		pageInfo.setUrl("readLink?pageIndex=");
+    private final static String CREATE = "admin/contact/link-create";
+    private final static String READ   = "admin/contact/link-read";
+    private final static String UPDATE = "admin/contact/link-update";
 
-		//当参数地址相同时pageInfo不用rc.setRequestAttr,但是严谨性,请将所需参数手动加入map
-		rc.setRequestAttr("pageInfo", pageInfo);
-		rc.setRequestAttr("links", links);
-		return READ;
-	}
-	
-	public String pu(WebContext rc) {
-		Link link = Link.INSTANCE.get(rc.getId());
-		rc.setRequestAttr("link", link);
-		rc.setRequestAttr("pageIndex", rc.getParam("pageIndex", 1));
-		return UPDATE;
-	}
-	
-	public String u(WebContext rc) throws IllegalAccessException, InvocationTargetException {
-		Link link = Link.INSTANCE.get(rc.getId());
-		rc.populate(link);
-		
-		link.updateAttrs(new String[]{"description", "name", "site", "status", "trash"}, new Object[]{
-				link.getDescription(), link.getName(), link.getSite(), link.getStatus(), link.getTrash()
-		});
-		
-		rc.setRequestAttr(Constants.OPERATION_SUCCESS, Constants.OPERATION_SUCCESS);
-		rc.setRequestAttr("link", link);
-		rc.setRequestAttr("pageIndex", rc.getParam("pageIndex", 1));
+    //private final static String DELETE = "admin/contact/link-read";
 
-		return UPDATE;
-	}
+    public String pc() {
+        return CREATE;
+    }
 
-	public String d(WebContext rc) throws IllegalAccessException, InvocationTargetException {
-		Link link = Link.INSTANCE.get(rc.getId());
-		link.delete();
-		
-		return r(rc);
-	}
+    public String c(WebContext rc) throws IllegalAccessException, InvocationTargetException {
+        Link link = new Link();
+        rc.populate(link);
+
+        if (link != null && link.getName() != null) {
+            link.setCreateTime(new Timestamp(System.currentTimeMillis()));
+            link.save();
+
+            rc.setRequestAttr(Constants.OPERATION_SUCCESS, Constants.OPERATION_SUCCESS);
+        }
+
+        return CREATE;
+    }
+
+    /**
+     * 
+     * 当参数地址相同时pageInfo不用rc.setRequestAttr,但是严谨性,请将所需参数手动加入map
+     * @param map
+     * @param pageInfo
+     * @return
+     * @throws InvocationTargetException 
+     * @throws IllegalAccessException 
+     */
+    public String r(WebContext rc) throws IllegalAccessException, InvocationTargetException {
+        PageInfo pageInfo = new PageInfo();
+        rc.populate(pageInfo);
+
+        List<Link> links = Link.INSTANCE.findPage(pageInfo, false);
+        pageInfo.setUrl("r?pageIndex=");
+
+        //当参数地址相同时pageInfo不用rc.setRequestAttr,但是严谨性,请将所需参数手动加入map
+        rc.setRequestAttr("pageInfo", pageInfo);
+        rc.setRequestAttr("links", links);
+        return READ;
+    }
+
+    public String pu(WebContext rc) {
+        Link link = Link.INSTANCE.get(rc.getId());
+        rc.setRequestAttr("link", link);
+        rc.setRequestAttr("pageIndex", rc.getParam("pageIndex", 1));
+        return UPDATE;
+    }
+
+    public String u(WebContext rc) throws IllegalAccessException, InvocationTargetException {
+        Link link = Link.INSTANCE.get(rc.getId());
+        rc.populate(link);
+
+        link.updateAttrs(new String[] { "description", "name", "site", "status", "trash" },
+            new Object[] { link.getDescription(), link.getName(), link.getSite(), link.getStatus(),
+                    link.getTrash() });
+
+        rc.setRequestAttr(Constants.OPERATION_SUCCESS, Constants.OPERATION_SUCCESS);
+        rc.setRequestAttr("link", link);
+        rc.setRequestAttr("pageIndex", rc.getParam("pageIndex", 1));
+
+        return UPDATE;
+    }
+
+    public String d(WebContext rc) throws IllegalAccessException, InvocationTargetException {
+        Link link = Link.INSTANCE.get(rc.getId());
+        link.delete();
+
+        return r(rc);
+    }
 }
--- src/main/java/com/ketayao/lucene/LuceneTasker.java
+++ src/main/java/com/ketayao/lucene/LuceneTasker.java
@@ -10,9 +10,10 @@
  *
  * </pre>
  **/
- 
+
 package com.ketayao.lucene;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -37,127 +38,134 @@ import com.ketayao.search.Searchable;
  */
 
 public class LuceneTasker extends TimerTask implements IndexTasker {
-	private static final Logger log = LoggerFactory.getLogger(LuceneTasker.class);
-	//private static int scheduleTime =  1000*60*5; // 五分钟
-	
-	private static String path = "D:\\fensy_lucene";
-	private static long scheduleTime = 1000*60*5; // 五分钟
-	
-	public LuceneTasker() {
-	}
-	
-	public LuceneTasker(String path, long scheduleTime) {
-		LuceneTasker.path = path;
-		LuceneTasker.scheduleTime = scheduleTime;		
-	}
-	
-	/**  
-	 * 返回 path 的值   
-	 * @return path  
-	 */
-	public String getPath() {
-		return path;
-	}
+    private static final Logger log          = LoggerFactory.getLogger(LuceneTasker.class);
+    //private static int scheduleTime =  1000*60*5; // 五分钟
 
-	/**  
-	 * 设置 path 的值  
-	 * @param path
-	 */
-	public void setPath(String path) {
-		LuceneTasker.path = path;
-	}
+    private static String       path         = "D:\\fensy_lucene";
+    private static long         scheduleTime = 1000 * 60 * 5;                              // 五分钟
 
-	/**  
-	 * 返回 scheduleTime 的值   
-	 * @return scheduleTime  
-	 */
-	public long getScheduleTime() {
-		return scheduleTime;
-	}
+    public LuceneTasker() {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
 
-	/**  
-	 * 设置 scheduleTime 的值  
-	 * @param scheduleTime
-	 */
-	public void setScheduleTime(long scheduleTime) {
-		LuceneTasker.scheduleTime = scheduleTime;
-	}
+    public LuceneTasker(String path, long scheduleTime) {
+        LuceneTasker.path = path;
+        LuceneTasker.scheduleTime = scheduleTime;
 
-	/**   
-	 * @return  
-	 * @see net.oschina.common.search.IndexTasker#list()  
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<IndexTask> list() {
-		long now = System.currentTimeMillis();
-		long before = now - scheduleTime;
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String sql = "modifyTime > '" + dateFormat.format(new Date(before)) + "'";
-		// 取得scheduleTime时间之前的数据
-		List<Article> addArticles = (List<Article>)Article.INSTANCE.list(sql);
-		
-		List<IndexTask> indexTasks = new ArrayList<IndexTask>();
-		for (final Article article : addArticles) {
-			indexTasks.add(new IndexTask() {
-				
-				@Override
-				public Searchable object() {
-					return article;
-				}
-				
-				@Override
-				public byte getOpt() {
-					if (article.getTrash() == 1 || !article.getStatus().equals(Article.Status.PUBLISH)) {
-						return IndexTask.OPT_DELETE;
-					}
-					
-					if (article.getPostTime().equals(article.getModifyTime())) {
-						return IndexTask.OPT_ADD;
-					}
-					
-					return IndexTask.OPT_UPDATE;
-				}
-				
-				@Override
-				public void afterBuild() {
-					if (log.isDebugEnabled()) {
-						String tmp = null;
-						if (getOpt() == 1) {
-							tmp = "添加索引";
-						} else if (getOpt() == 2) {
-							tmp = "更新索引";
-						} else if (getOpt() == 4) {
-							tmp = "删除索引";
-						}
-						log.debug("articleId=" + article.getId() + ",OPT=" + tmp);
-					}
-				}
-			});
-		}
-		
-		return indexTasks;
-	}
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
 
-	/**   
-	 *   
-	 * @see java.util.TimerTask#run()  
-	 */
-	@Override
-	public void run() {
-		try {
-			IndexUpdater.execute(new String[]{
-					"-p", getPath(),
-					"-t", this.getClass().getName()
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		Timer timer = new Timer();
-		LuceneTasker tasker = new LuceneTasker();
-		timer.schedule(tasker, 0, tasker.getScheduleTime());
-	}
+    /**  
+     * 返回 path 的值   
+     * @return path  
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**  
+     * 设置 path 的值  
+     * @param path
+     */
+    public void setPath(String path) {
+        LuceneTasker.path = path;
+    }
+
+    /**  
+     * 返回 scheduleTime 的值   
+     * @return scheduleTime  
+     */
+    public long getScheduleTime() {
+        return scheduleTime;
+    }
+
+    /**  
+     * 设置 scheduleTime 的值  
+     * @param scheduleTime
+     */
+    public void setScheduleTime(long scheduleTime) {
+        LuceneTasker.scheduleTime = scheduleTime;
+    }
+
+    /**   
+     * @return  
+     * @see net.oschina.common.search.IndexTasker#list()  
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<IndexTask> list() {
+        long now = System.currentTimeMillis();
+        long before = now - scheduleTime;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String sql = "modifyTime > '" + dateFormat.format(new Date(before)) + "'";
+        // 取得scheduleTime时间之前的数据
+        List<Article> addArticles = (List<Article>) Article.INSTANCE.list(sql);
+
+        List<IndexTask> indexTasks = new ArrayList<IndexTask>();
+        for (final Article article : addArticles) {
+            indexTasks.add(new IndexTask() {
+
+                @Override
+                public Searchable object() {
+                    return article;
+                }
+
+                @Override
+                public byte getOpt() {
+                    if (article.getTrash() == 1
+                        || !article.getStatus().equals(Article.Status.PUBLISH)) {
+                        return IndexTask.OPT_DELETE;
+                    }
+
+                    if (article.getPostTime().equals(article.getModifyTime())) {
+                        return IndexTask.OPT_ADD;
+                    }
+
+                    return IndexTask.OPT_UPDATE;
+                }
+
+                @Override
+                public void afterBuild() {
+                    if (log.isDebugEnabled()) {
+                        String tmp = null;
+                        if (getOpt() == 1) {
+                            tmp = "添加索引";
+                        } else if (getOpt() == 2) {
+                            tmp = "更新索引";
+                        } else if (getOpt() == 4) {
+                            tmp = "删除索引";
+                        }
+                        log.debug("articleId=" + article.getId() + ",OPT=" + tmp);
+                    }
+                }
+            });
+        }
+
+        return indexTasks;
+    }
+
+    /**   
+     *   
+     * @see java.util.TimerTask#run()  
+     */
+    @Override
+    public void run() {
+        try {
+            IndexUpdater.execute(new String[] { "-p", getPath(), "-t", this.getClass().getName() });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        LuceneTasker tasker = new LuceneTasker();
+        timer.schedule(tasker, 0, tasker.getScheduleTime());
+    }
 }
